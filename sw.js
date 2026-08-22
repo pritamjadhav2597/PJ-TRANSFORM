72f8774d4d3180954d703d2ce0c423e6f5d85571
pritamjadhav2597 <pritamjadhav2597@gmail.com>
2026-08-22 18:32:31 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker for Personal Transformation PWA
 // Bump this version any time app files change, to force cache refresh.
-const CACHE_VERSION = 'v23';
+const CACHE_VERSION = 'v24';
 const CACHE_NAME = `transform-app-${CACHE_VERSION}`;
 
 const PRECACHE_URLS = [
